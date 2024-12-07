358febf71b496664532116cbbed9f4e9bb28d7fb
nguyentito10 <tientoan91002003@gmail.com>
2024-12-08 01:11:01 +0700

    Update: Completed function drv_bmp180_write_reg

--- main/drv_bmp180.c
+++ main/drv_bmp180.c
@@ -47,11 +47,11 @@ esp_err_t bmp180_write_register(uint8_t reg_addr, uint8_t *data, size_t len)
 }
 
 
-drv_bmp180_ret_t drv_bmp180_write_reg (uint8_t* command, size_t length)
+static drv_bmp180_ret_t drv_bmp180_write_reg (uint8_t* command)
 {
   bool ret = false;                                     
-  ret = bsp_i2c_write_mem((BMP180_SENSOR_ADDR << 1) | I2C_MASTER_WRITE, DATA_MODE, data, length);
-  return (ret == true) ? ESP_OK : ESP_FAIL;
+  ret = bsp_i2c_write_mem((BMP180_SENSOR_ADDR << 1) | I2C_MASTER_WRITE, BMP180_REG_CTRL_VALUE, command, 1);
+  return (ret == true) ? DRV_BMP180_ERROR : DRV_BMP180_OK;
 }
 
 // Read from BMP180 register
