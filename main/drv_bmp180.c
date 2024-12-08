2ef125f286a861797622e0db90cd8454ac8d1022
nguyentito10 <tientoan91002003@gmail.com>
2024-12-08 21:22:12 +0700

    Refactor: change the type function read raw press to int32_t

--- main/drv_bmp180.c
+++ main/drv_bmp180.c
@@ -43,7 +43,7 @@ static drv_bmp180_ret_t drv_bmp180_read_calibration (void);
 static int32_t drv_bmp180_read_raw_temperature      (void);
 
 // Read raw pressure data from BMP180
-static drv_bmp180_ret_t drv_bmp180_read_raw_pressure(int32_t *raw_press);
+static int32_t drv_bmp180_read_raw_pressure         (void);
 
 // Temperature compensation function
 static int32_t drv_bmp180_calculate_temp(int32_t raw_temp);
